7617c0d1908f79278d7c65025b3c7c0ef9204c35
samwhelp <samwhelp@users.noreply.github.com>
2025-06-01 00:00:03 +0800

    fix

--- template/cycle/helper/stage/module/base-network-config-install/install.sh
+++ template/cycle/helper/stage/module/base-network-config-install/install.sh
@@ -44,58 +44,75 @@ REF_INIT_DIR_PATH="${REF_BASE_DIR_PATH}/../../../ext"
 
 
 ################################################################################
-### Head: Model / mod_module_locale_config
+### Head: Model / mod_module_network_config_install
 ##
 
-sys_locale_config_locale_conf_via_cmd () {
+sys_network_config_install_network_manager_via_cmd () {
 
-	local the_locale_lang="LANG=en_US.UTF-8"
+
+
+
+cat << __EOF__ > /etc/NetworkManager/NetworkManager.conf
+[main]
+rc-manager=resolvconf
+plugins=ifupdown,keyfile
+dns=dnsmasq
+
+[ifupdown]
+managed=false
+__EOF__
+
+
+
 
 	util_error_echo
-	util_error_echo echo "${the_locale_lang}" '|' tee /etc/locale.conf
+	util_error_echo cat /etc/NetworkManager/NetworkManager.conf
 	util_error_echo
-	echo "${the_locale_lang}" | tee /etc/locale.conf 2>&1 >/dev/null
+	cat /etc/NetworkManager/NetworkManager.conf
 
-
-	return 0
-}
-
-sys_locale_config_locale_gen_via_cmd () {
-
-	local the_locale_gen="en_US.UTF-8 UTF-8"
-
-	util_error_echo
-	util_error_echo echo "${the_locale_gen}" '|' tee -a /etc/locale.gen
-	util_error_echo
-	echo "${the_locale_gen}" | tee -a /etc/locale.gen 2>&1 >/dev/null
-
-
-
-
-	the_locale_gen="C.UTF-8 UTF-8"
-
-	util_error_echo
-	util_error_echo echo "${the_locale_gen}" '|' tee -a /etc/locale.gen
-	util_error_echo
-	echo "${the_locale_gen}" | tee -a /etc/locale.gen 2>&1 >/dev/null
 
 
 
 	return 0
 }
 
-sys_locale_config_via_cmd () {
+sys_network_config_install_netplan_via_cmd () {
 
 
-	sys_locale_config_locale_conf_via_cmd
 
-	sys_locale_config_locale_gen_via_cmd
+
+cat << __EOF__ > /etc/netplan/01-network-manager-all.yaml
+network:
+  version: 2
+  renderer: NetworkManager
+__EOF__
+
+
+
+
+	util_error_echo
+	util_error_echo cat /etc/netplan/01-network-manager-all.yaml
+	util_error_echo
+	cat /etc/netplan/01-network-manager-all.yaml
+
+
 
 
 	return 0
 }
 
-sys_locale_config_via_file () {
+sys_network_config_install_via_cmd () {
+
+
+	sys_network_config_install_network_manager_via_cmd
+
+	sys_network_config_install_netplan_via_cmd
+
+
+	return 0
+}
+
+sys_network_config_install_via_file () {
 
 
 	local source_dir_path="${REF_BASE_DIR_PATH}/asset/overlay"
@@ -110,48 +127,21 @@ sys_locale_config_via_file () {
 	return 0
 }
 
-sys_locale_gen () {
+
+mod_module_network_config_install () {
+
+	sys_network_config_install_via_cmd
+
+	#sys_network_config_install_via_file
 
 
-	util_error_echo
-	util_error_echo locale-gen
-	util_error_echo
-	locale-gen
-
-
-	return 0
-}
-
-sys_locale_update () {
-
-	local the_lang="en_US.UTF-8"
-
-	util_error_echo
-	util_error_echo update-locale LANG=${the_lang} LC_ALL=${the_lang}
-	util_error_echo
-	update-locale LANG=${the_lang} LC_ALL=${the_lang}
-
-
-	return 0
-}
-
-mod_module_locale_config () {
-
-	#sys_locale_config_via_cmd
-
-	sys_locale_config_via_file
-
-
-
-
-	sys_locale_gen
 
 
 	return 0
 }
 
 ##
-### Tail: Model / mod_module_locale_config
+### Tail: Model / mod_module_network_config_install
 ################################################################################
 
 
@@ -172,7 +162,7 @@ portal_install () {
 	util_error_echo "[Run Module]: ${script_file_path}"
 
 
-	mod_module_locale_config
+	mod_module_network_config_install
 
 
 }
--- template/cycle/helper/stage/module/base-network-package-install/install.sh
+++ template/cycle/helper/stage/module/base-network-package-install/install.sh
@@ -44,114 +44,40 @@ REF_INIT_DIR_PATH="${REF_BASE_DIR_PATH}/../../../ext"
 
 
 ################################################################################
-### Head: Model / mod_module_locale_config
+### Head: Model / mod_module_network_package_install
 ##
 
-sys_locale_config_locale_conf_via_cmd () {
-
-	local the_locale_lang="LANG=en_US.UTF-8"
-
-	util_error_echo
-	util_error_echo echo "${the_locale_lang}" '|' tee /etc/locale.conf
-	util_error_echo
-	echo "${the_locale_lang}" | tee /etc/locale.conf 2>&1 >/dev/null
+mod_module_network_package_install () {
 
 
-	return 0
-}
-
-sys_locale_config_locale_gen_via_cmd () {
-
-	local the_locale_gen="en_US.UTF-8 UTF-8"
-
-	util_error_echo
-	util_error_echo echo "${the_locale_gen}" '|' tee -a /etc/locale.gen
-	util_error_echo
-	echo "${the_locale_gen}" | tee -a /etc/locale.gen 2>&1 >/dev/null
+##
+## ## Link
+##
+## * https://github.com/mvallim/live-custom-ubuntu-from-scratch/blob/master/scripts/chroot_build.sh#L101
+##
 
 
-
-
-	the_locale_gen="C.UTF-8 UTF-8"
-
-	util_error_echo
-	util_error_echo echo "${the_locale_gen}" '|' tee -a /etc/locale.gen
-	util_error_echo
-	echo "${the_locale_gen}" | tee -a /etc/locale.gen 2>&1 >/dev/null
-
-
-
-	return 0
-}
-
-sys_locale_config_via_cmd () {
-
-
-	sys_locale_config_locale_conf_via_cmd
-
-	sys_locale_config_locale_gen_via_cmd
-
-
-	return 0
-}
-
-sys_locale_config_via_file () {
-
-
-	local source_dir_path="${REF_BASE_DIR_PATH}/asset/overlay"
-	local target_dir_path="/"
-
-	util_error_echo
-	util_error_echo cp -rfT "${source_dir_path}" "${target_dir_path}"
-	util_error_echo
-	cp -rfT "${source_dir_path}" "${target_dir_path}"
-
-
-	return 0
-}
-
-sys_locale_gen () {
+local run_cmd=$(cat << __EOF__
+	apt-get install -y --no-install-recommends
+		network-manager
+		net-tools
+		resolvconf
+__EOF__
+)
 
 
 	util_error_echo
-	util_error_echo locale-gen
+	util_error_echo $run_cmd
 	util_error_echo
-	locale-gen
 
-
-	return 0
-}
-
-sys_locale_update () {
-
-	local the_lang="en_US.UTF-8"
-
-	util_error_echo
-	util_error_echo update-locale LANG=${the_lang} LC_ALL=${the_lang}
-	util_error_echo
-	update-locale LANG=${the_lang} LC_ALL=${the_lang}
-
-
-	return 0
-}
-
-mod_module_locale_config () {
-
-	#sys_locale_config_via_cmd
-
-	sys_locale_config_via_file
-
-
-
-
-	sys_locale_gen
+	$run_cmd
 
 
 	return 0
 }
 
 ##
-### Tail: Model / mod_module_locale_config
+### Tail: Model / mod_module_network_package_install
 ################################################################################
 
 
@@ -172,7 +98,7 @@ portal_install () {
 	util_error_echo "[Run Module]: ${script_file_path}"
 
 
-	mod_module_locale_config
+	mod_module_network_package_install
 
 
 }

5179a1125ecf294ef17218a8ad31c0c396d90fdb
samwhelp <samwhelp@users.noreply.github.com>
2025-05-31 08:07:57 +0800

    fix

---
+++ template/cycle/helper/stage/module/base-machine-id-config/install.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+
+
+################################################################################
+### Head: Note
+##
+
+##
+## ## Link
+##
+## * https://github.com/samwhelp/skel-project-plan/blob/master/develop/sh/project-helper/basic/helper/bin/prepare.sh
+##
+
+##
+### Tail: Note
+################################################################################
+
+
+################################################################################
+### Head: Init
+##
+
+REF_CMD_FILE_NAME="$(basename "${0}")"
+REF_BASE_DIR_PATH="$(cd -- "$(dirname -- "${0}")" ; pwd)"
+REF_INIT_DIR_PATH="${REF_BASE_DIR_PATH}/../../../ext"
+. "${REF_INIT_DIR_PATH}/init.sh"
+
+##
+### Tail: Init
+################################################################################
+
+
+################################################################################
+### Head: Model / mod_module_machine_id_config
+##
+
+mod_module_machine_id_config () {
+
+	local host_name="${REF_BUILD_SUBJECT_NAME}"
+
+	util_error_echo
+	util_error_echo dbus-uuidgen '|' sudo tee /etc/machine_id
+	util_error_echo
+	dbus-uuidgen | sudo tee /etc/machine_id 2>&1 >/dev/null
+
+
+	util_error_echo
+	util_error_echo ln -fs /etc/machine-id /var/lib/dbus/machine-id
+	util_error_echo
+	ln -fs /etc/machine-id /var/lib/dbus/machine-id
+
+
+	return 0
+}
+
+##
+### Tail: Model / mod_module_machine_id_config
+################################################################################
+
+
+################################################################################
+### Head: Portal / portal_install
+##
+
+portal_install () {
+
+	util_error_echo
+	util_error_echo "##"
+	util_error_echo "## ## Run Module"
+	util_error_echo "##"
+	util_error_echo
+
+	local script_file_path="${REF_BASE_DIR_PATH}/${REF_CMD_FILE_NAME}"
+
+	util_error_echo "[Run Module]: ${script_file_path}"
+
+
+	mod_module_machine_id_config
+
+
+}
+
+##
+### Tail: Portal / portal_install
+################################################################################
+
+
+################################################################################
+### Head: Main
+##
+
+__main__ () {
+
+	portal_install "${@}"
+
+}
+
+__main__ "${@}"
+
+##
+### Tail: Main
+################################################################################

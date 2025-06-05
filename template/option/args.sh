f43edd9ce47ab776c38f9aa0b835373062955198
samwhelp <samwhelp@users.noreply.github.com>
2025-06-05 20:15:11 +0800

    fix

--- template/option/args.sh
+++ template/option/args.sh
@@ -101,6 +101,37 @@ REF_BUILD_LANG_MODE="en_US"
 ##
 ## ## REF_BUILD_LANG
 ##
+## > for /etc/locale.conf
 ##
 
 REF_BUILD_LANG="en_US.UTF-8"
+
+
+
+
+##
+## ## REF_BUILD_LOCALE_GEN
+##
+## > for /etc/locale.gen
+##
+
+REF_BUILD_LOCALE_GEN="zh_TW.UTF-8 zh_CN.UTF-8 zh_HK.UTF-8 ja_JP.UTF-8 ko_KR.UTF-8 en_US.UTF-8"
+
+
+
+
+##
+## ## REF_BUILD_TIMEZONE
+##
+## > for /etc/localtime
+##
+## > ls -1 /usr/share/zoneinfo/
+##
+## > tree /usr/share/zoneinfo/
+##
+
+## /usr/share/zoneinfo/Etc/UTC
+REF_BUILD_TIMEZONE="Etc/UTC"
+
+## /usr/share/zoneinfo/Asia/Taipei
+#REF_BUILD_TIMEZONE="Asia/Taipei"

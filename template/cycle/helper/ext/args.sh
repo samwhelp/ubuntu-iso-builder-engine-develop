b04fa7309842b62b378fdd8e3e0b15e5b1c27c4b
samwhelp <samwhelp@users.noreply.github.com>
2025-06-10 13:40:53 +0800

    fix

--- template/cycle/helper/ext/args.sh
+++ template/cycle/helper/ext/args.sh
@@ -58,7 +58,7 @@ args_var_dump () {
 	util_debug_echo
 
 
-	util_debug_echo "REF_BUILD_LANG_PACK_CODE=${REF_BUILD_LANG_PACK_CODE}"
+	util_debug_echo "REF_BUILD_LOCALE_NAME_PART_LANGUAGE=${REF_BUILD_LOCALE_NAME_PART_LANGUAGE}"
 	util_debug_echo "REF_BUILD_LANG_MODE=${REF_BUILD_LANG_MODE}"
 	util_debug_echo "REF_BUILD_LANG=${REF_BUILD_LANG}"
 	util_debug_echo
